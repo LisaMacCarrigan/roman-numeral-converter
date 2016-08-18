ac8bd305520c22d837dc83b4b711814fedb4e86b
Lisa MacCarrigan and Sam Peppard <sam@>
2016-08-18 11:30:31 -0400

    add js for detection of invalid numbers

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,14 @@ $(document).ready(function() {
 var romanSymbols = ["I", "V", "X", "L", "C", "D", "M"];
 var romanValues = [1, 5, 10, 50, 100, 500, 1000];
 
-var toRoman = function(number) {
-  
 
+var toRoman = function(num) {
+
+  // Do nothing to 0.
+  // Do nothing to numbers higher than 3,999.
+  if (num === 0 || num > 3999) {
+    console.log("Please enter a number greater than 0 and less than 4,000");
+  }
 }
 
 
