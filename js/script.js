e230a980adbe781a32eaf001c1e9e391b6ca19c3
Lisa MacCarrigan and Sam Peppard <lisa@>
2016-08-18 16:02:11 -0400

    add JS to display returned result

--- js/script.js
+++ js/script.js
@@ -1,32 +1,24 @@
 $(document).ready(function() {
 // Business Logic
-
-
 var toRoman = function(num) {
 
-  var romanSymbols = ["I", "V", "X", "L", "C", "D", "M"];
   var romanValOnes = ["", "I","II","III","IV","V","VI","VII","VIII","IX"];
   var romanValTens = ["", "X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
   var romanValHundreds = ["", "C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
   var romanValThou = ["", "M","MM","MMM","Mv"];
+  var testNum = num.toString().split("");
+  var romanNumeral = [];
 
   // Do nothing to 0.
   // Do nothing to numbers higher than 3,999.
   if (num === 0 || num > 3999) {
-    console.log("Please enter a number greater than 0 and less than 4,000");
+    alert("Please enter a number greater than 0 and less than 4,000");
   }
   else {
-    var testNum = num.toString().split("");
-    var romanNumeral = [];
-    var result = romanNumeral.join("");
-
     // NUMBERS IN THOUSANDS
     if (testNum.length === 4) {
-      console.log(testNum.length);
-
       for (var m = 0; m < romanValThou.length; m++) {
         if (testNum[0] == m) {
-          console.log(romanValThou[m]);
           romanNumeral.push(romanValThou[m]);
         }
       }
@@ -48,8 +40,6 @@ var toRoman = function(num) {
     }
     // NUMBERS IN HUNDREDS
     else if (testNum.length === 3) {
-      console.log(testNum.length);
-
       for (var c = 0; c < romanValHundreds.length; c++) {
         if (testNum[0] == c) {
           romanNumeral.push(romanValHundreds[c]);
@@ -65,12 +55,9 @@ var toRoman = function(num) {
           romanNumeral.push(romanValOnes[u]);
         }
       }
-      console.log(romanNumeral);
     }
     // NUMBERS IN TENS
     else if (testNum.length === 2) {
-      console.log(testNum.length);
-
       for (var d = 0; d < romanValTens.length; d++) {
         if (testNum[0] == d) {
           romanNumeral.push(romanValTens[d]);
@@ -81,22 +68,19 @@ var toRoman = function(num) {
           romanNumeral.push(romanValOnes[u]);
         }
       }
-      console.log(romanNumeral);
     }
     // NUMBERS IN ONES
     else if (testNum.length === 1) {
-      console.log(testNum.length);
       for (var u = 0; u < romanValOnes.length; u++) {
         if (testNum[0] == u) {
           romanNumeral.push(romanValOnes[u]);
         }
       }
-      console.log(romanNumeral);
-    }
-    else {
-      console.log("Your number is too large.")
     }
 
+    var result = romanNumeral.join("");
+    console.log(result);
+    return result;
 
   }
 
@@ -108,7 +92,7 @@ var toRoman = function(num) {
     //grab user input
     var number = parseInt($("#input-number").val());
     $("#converted-number").text("");
-    toRoman(number);
-    //$("#converted-number").text(number);
+    var romanNumeralResult = toRoman(number);
+    $("#converted-number").text(romanNumeralResult);
   });
 });
